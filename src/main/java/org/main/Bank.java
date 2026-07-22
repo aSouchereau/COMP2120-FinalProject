fca2a4eff008e9d8188f590d6d46ea8b45ba1fac
tylerzanutto <tyezanutto@gmail.com>
2026-07-22 18:43:44 -0400

    Update Bank.java to work with accounts

--- src/main/java/org/main/Bank.java
+++ src/main/java/org/main/Bank.java
@@ -5,10 +5,16 @@ import java.util.List;
 
 public class Bank {
     private static final List<User> users = new ArrayList<>();
-
+    private static final List<Account> account = new ArrayList<>();
+    
     public static void addUser(User user) {
         users.add(user);
         System.out.println("User added: " + user);
+    }
+
+    public static void addAccount(Account account) {
+        accounts.add(account);
+        System.out.println("Account added: " + account.getAccountNumber());
     }
 
     public static List<User> getUsers() {
